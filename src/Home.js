76b34f99a29e25f7951d33fcd7092c6618b2d2c9
cyberjam <jamsince1994@naver.com>
2023-07-08 23:11:58 +0900

    REFACTOR: 데이러 불러오는 로직 파일 분리

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import BankInfo from "./BankInfo";
+import fetchBankData from "./api/fetchBankData";
 import { createFuzzyMatcher } from "./utils/fuzzyMatcher";
 
 function Home() {
@@ -7,26 +8,6 @@ function Home() {
   const [targetBank, setTargetBank] = useState("");
   const [targetBankInfo, setTargetBankInfo] = useState();
   const [searchBankInfos, setSearchBankInfos] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbxfKYenkGNxchOPPRYeq50C42GBpa6WjRWDiuZDbwEfUm6QlKZkpPnmVVqhrKyxLgc7/exec"
-      );
-
-      if (!response.ok) {
-        throw new Error("데이터를 가져오는데 실패했습니다.");
-      }
-
-      const data = await response.json();
-      setBankInfos(data);
-    } catch (error) {
-      alert(
-        `데이터 로딩에 실패하였습니다. 새로고침 하더라도 문제가 있다면 에러메세지와 함께 문의주세요 : ${error.message}`
-      );
-      console.log(`데이터 요청 오류: ${error.message}`);
-    }
-  };
 
   const handleSearch = (event) => {
     event.preventDefault();
@@ -44,8 +25,13 @@ function Home() {
     setTargetBank(value);
   };
 
+  const fetchBankInfos = async () => {
+    const data = await fetchBankData();
+    setBankInfos(data);
+  };
+
   useEffect(() => {
-    getData();
+    fetchBankInfos();
   }, []);
 
   useEffect(() => {
