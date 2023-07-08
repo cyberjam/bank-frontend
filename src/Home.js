302cf9443e1073c903fdcd42f0a829c73afa68a8
cyberjam <jamsince1994@naver.com>
2023-07-08 22:58:17 +0900

    REFACTOR: 데이터 로딩 실패시 예외 추가

--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,23 @@ function Home() {
   const [searchBankInfos, setSearchBankInfos] = useState([]);
 
   const getData = async () => {
-    const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbxfKYenkGNxchOPPRYeq50C42GBpa6WjRWDiuZDbwEfUm6QlKZkpPnmVVqhrKyxLgc7/exec"
-    );
-    const data = await response.json();
-    setBankInfos(data);
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbxfKYenkGNxchOPPRYeq50C42GBpa6WjRWDiuZDbwEfUm6QlKZkpPnmVVqhrKyxLgc7/exec"
+      );
+
+      if (!response.ok) {
+        throw new Error("데이터를 가져오는데 실패했습니다.");
+      }
+
+      const data = await response.json();
+      setBankInfos(data);
+    } catch (error) {
+      alert(
+        `데이터 로딩에 실패하였습니다. 새로고침 하더라도 문제가 있다면 에러메세지와 함께 문의주세요 : ${error.message}`
+      );
+      console.log(`데이터 요청 오류: ${error.message}`);
+    }
   };
 
   const handleSearch = (event) => {
