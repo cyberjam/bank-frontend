9e9f77dcb4b648d018836154f6f03e0093624994
cyberjam <jamsince1994@naver.com>
2023-07-11 18:50:13 +0900

    FIX: 데이터 로딩 후 placeholder 제거

--- src/components/BankInput.js
+++ src/components/BankInput.js
@@ -14,11 +14,7 @@ function BankInput({ bankInfos, targetBank, setTargetBank }) {
         value={targetBank}
         onChange={handleInputBank}
         disabled={bankInfos.length ? "" : "disabled"}
-        placeholder={
-          bankInfos.length
-            ? "금고 이름을 입력해주세요"
-            : "실시간 데이터 로딩중 :)"
-        }
+        placeholder={bankInfos.length ? "" : "실시간 데이터 로딩중 :)"}
       ></input>
     </form>
   );
